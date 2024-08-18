b1e1b6934f3f2ce2c6d899a17ef6480da296b4ad
mad-hero <akotyle@gmail.com>
2024-08-18 17:23:59 -0400

    Definir readme y datos del grupo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,17 +4,17 @@
 
 <head>
   <meta charset="UTF-8">
-  <title>Desarrollo Software Web - Unidad 2</title>
+  <title>Desarrollo Software Web I - Evaluación Unidad 2</title>
   @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
 
 <body style="padding: 20px">
-<h1>Desarrollo Software Web - Unidad 2</h1>
+<h1>Desarrollo Software Web I - Evaluación Unidad 2</h1>
 <legend>Grupo Kimera - Integrantes:</legend>
 <ul class="list-group" style="max-width: 300px">
   <li class="list-group-item">Mario Quevedo Astudillo</li>
+  <li class="list-group-item">Javier Reumay Ancalaf - Sec 71</li>
   <li class="list-group-item">Franco Vásquez Fuentes</li>
-  <li class="list-group-item">Javier Reumay Ancalaf</li>
 </ul>
 
 <h6 style="margin-top: 30px">Repositorio: <a target="_blank" href="https://github.com/mbstnd/01-laravel">https://github.com/mbstnd/01-laravel</a></h6>
